654bb047659aedb974fad14c3417c52a055c8b19
RockTroll <janbert.jadraque@gmail.com>
2017-10-12 14:54:32 +0800

    My Django Girls app, first commit

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jadraquej\desktop\old\console2\tutorial\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
